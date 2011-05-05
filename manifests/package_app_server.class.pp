49ad152144dca598f2307e05e2d86eba9e35901d
Andreas Hucks <andreas.hucks@duochrome.net>
2011-05-05 11:49:25 +0200

    Switched PHPUnit to Pear installation to allow for more recent versions

--- manifests/package_app_server.class.pp
+++ manifests/package_app_server.class.pp
@@ -85,9 +85,70 @@ class package_app_server
 
   package
   {
-    'phpunit':
+    'php-pear':
       ensure  => present,
       require => Package['php5-cli']
+  }
+
+  file
+  {
+    'pear.tmpdirfix.prepare':
+      ensure  => directory,
+      path    => '/tmp/pear',
+      require => Package['php5-cli']
+  }
+
+  file
+  {
+    'pear.tmpdirfix':
+      ensure  => directory,
+      path    => '/tmp/pear/cache',
+      mode    => 777,
+      require => File['pear.tmpdirfix.prepare']
+  }
+
+  exec
+  {
+    'pear.upgrade':
+      path => '/bin:/usr/bin:/usr/sbin',
+      command => 'pear upgrade PEAR',
+      require => File['pear.tmpdirfix']
+  }
+
+  exec
+  {
+    'phpunit.prepare1':
+      unless => 'pear channel-info pear.phpunit.de',
+      path => '/bin:/usr/bin:/usr/sbin',
+      command => 'pear channel-discover pear.phpunit.de',
+      require => Exec['pear.upgrade']
+  }
+
+  exec
+  {
+    'phpunit.prepare2':
+      unless => 'pear channel-info pear.symfony-project.com',
+      path => '/bin:/usr/bin:/usr/sbin',
+      command => 'pear channel-discover pear.symfony-project.com',
+      require => Exec['phpunit.prepare1']
+  }
+
+  exec
+  {
+    'phpunit.prepare3':
+      unless => 'pear channel-info components.ez.no',
+      path => '/bin:/usr/bin:/usr/sbin',
+      command => 'pear channel-discover components.ez.no',
+      require => Exec['phpunit.prepare2']
+  }
+
+  exec
+  {
+    'phpunit.install':
+      unless => 'pear info phpunit/PHPUnit',
+      path => '/bin:/usr/bin:/usr/sbin',
+      command => 'pear install phpunit/PHPUnit --alldeps',
+      require => Exec['phpunit.prepare3']
   }
 
 }
